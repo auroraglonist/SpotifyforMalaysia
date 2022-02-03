124ca98a1e08db4de9efe6955682d43993383de8
Aurora <olafrobin@yandex.com>
2022-02-03 07:38:10 -0600

    Build frontend

---
+++ public/precache-manifest.0594fb0a2cf9def1fc297ae47f33b7cb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4419222c5fe613a4cbfe990c5af0aa9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8cb28b371cbbae342147",
+    "url": "/static/css/2.f1d36a8f.chunk.css"
+  },
+  {
+    "revision": "b06326d2a7067263b23c",
+    "url": "/static/css/main.ac0ebeeb.chunk.css"
+  },
+  {
+    "revision": "8cb28b371cbbae342147",
+    "url": "/static/js/2.6cd28f5c.chunk.js"
+  },
+  {
+    "revision": "b06326d2a7067263b23c",
+    "url": "/static/js/main.941772d6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d350ce249a30596db0ed98838873a907",
+    "url": "/static/media/Comfortaa-Regular.d350ce24.ttf"
+  },
+  {
+    "revision": "debad321a6e0ee9b8414733ad34db43d",
+    "url": "/static/media/shazam.debad321.png"
+  },
+  {
+    "revision": "fb2192a677f1db6030f0ed2d1ba2f0d8",
+    "url": "/static/media/trackIcon.fb2192a6.png"
+  }
+]);
--- server.js
+++ server.js
@@ -164,9 +164,13 @@ if (process.env.NODE_ENV === 'production') {
     res.sendfile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   })
 } else {
-  app.use(express.static(path.join(__dirname, '/frontend/public')))
+//  app.use(express.static(path.join(__dirname, './public')))
+//  app.get('*', (req, res) => {
+//    res.sendFile(path.join(__dirname, './public/index.html'))
+//  })
+  app.use(express.static('./build'))
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './frontend/public/index.html'))
+    res.sendfile(path.resolve(__dirname, './public/index.html'))
   })
 }
 
